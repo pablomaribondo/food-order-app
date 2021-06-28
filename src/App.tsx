719f9be32c0bed8bf6a74dca9c9c21f430209894
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-28 11:17:06 -0300

    feat: create Meals component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { FC } from 'react';
 
 import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 import './App.css';
 
 const App: FC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
 };
 
 export default App;
