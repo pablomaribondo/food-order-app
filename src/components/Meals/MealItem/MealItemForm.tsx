940e3e23d7847a2a6ca152d55c1c4d727007dac8
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-29 13:28:32 -0300

    feat: adjust MealItem component to pass ref to Input

--- src/components/Meals/MealItem/MealItemForm.tsx
+++ src/components/Meals/MealItem/MealItemForm.tsx
@@ -1,16 +1,41 @@
-import { FC } from 'react';
+import { FC, FormEvent, Ref, useRef, useState } from 'react';
 
-import Input from '../../UI/Input';
+import Input, { InputRef } from '../../UI/Input';
 import styles from './MealItemForm.module.css';
 
 interface MealItemFormProps {
   id: string;
+  onAddToCart: (amount: number) => void;
 }
 
-const MealItemForm: FC<MealItemFormProps> = ({ id }) => {
+const MealItemForm: FC<MealItemFormProps> = ({ id, onAddToCart }) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef<InputRef>();
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (amountInputRef && amountInputRef.current) {
+      const enteredAmount = amountInputRef.current.value;
+      const enteredAmountNumber = parseInt(enteredAmount, 10);
+
+      if (
+        enteredAmount.trim().length === 0 ||
+        enteredAmountNumber < 1 ||
+        enteredAmountNumber > 5
+      ) {
+        setAmountIsValid(false);
+        return;
+      }
+
+      onAddToCart(enteredAmountNumber);
+    }
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef as Ref<InputRef>}
         label="Amount"
         id={`amount_${id}`}
         type="number"
@@ -20,6 +45,7 @@ const MealItemForm: FC<MealItemFormProps> = ({ id }) => {
         defaultValue="1"
       />
       <button type="submit">+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
     </form>
   );
 };
