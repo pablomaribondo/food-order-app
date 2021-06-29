5a41a0440398663128831a2a76f9838b57d5171a
Pablo Maribondo <pablomgvieira@gmail.com>
2021-06-29 13:29:30 -0300

    feat: add animation and dynamic values to Header

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart.context';
 import styles from './HeaderCartButton.module.css';
 
 interface HeaderCartButtonProps {
@@ -8,13 +9,40 @@ interface HeaderCartButtonProps {
 }
 
 const HeaderCartButton: FC<HeaderCartButtonProps> = ({ onClick }) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+  const cartContext = useContext(CartContext);
+
+  const numberOfCartItems = cartContext.items.reduce(
+    (accumulator, item) => accumulator + item.amount,
+    0
+  );
+
+  const buttonClasses = `${styles.button} ${
+    buttonIsHighlighted ? styles.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return;
+    }
+
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} type="button" onClick={onClick}>
+    <button className={buttonClasses} type="button" onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
