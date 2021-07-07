4277e445cc9af5b847932ef60b624a381f6a9a84
Pablo Maribondo <pablomgvieira@gmail.com>
2021-07-07 09:54:50 -0300

    feat: add error handler

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -8,12 +8,18 @@ import { Meal } from '../../store/types';
 const AvailableMeals: FC = () => {
   const [meals, setMeals] = useState<Meal[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     (async () => {
       const response = await fetch(
         `${process.env.REACT_APP_DB_URL}/meals.json`
       );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData: { [key: string]: Meal } = await response.json();
 
       const loadedMeals: Meal[] = Object.entries(responseData).map(
@@ -27,13 +33,24 @@ const AvailableMeals: FC = () => {
 
       setMeals(loadedMeals);
       setIsLoading(false);
-    })();
+    })().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section className={styles.mealsLoading}>
         <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={styles.mealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
